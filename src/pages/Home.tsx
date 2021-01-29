44a2e138b5e1753193427cab2b94ac0a070727bc
leularia <leulsegedtariku@gmail.com>
2021-01-30 01:53:54 +0300

    fix err unused ts vars

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import "./Home.css";
 
 const Home = () => {
-  const history = useHistory();
+  // const history = useHistory();
 
 	const newList = () => {
 		
